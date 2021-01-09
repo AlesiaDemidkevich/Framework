c3d194f4f5560547f382b80062ccb336d031b1a4
alesyademidkevich01 <alesya.demidkevich@mail.ru>
2021-01-09 21:50:33 +0300

    new

--- src/test/java/service/ItemCreator.java
+++ src/test/java/service/ItemCreator.java
@@ -5,7 +5,6 @@ import model.Item;
 public class ItemCreator {
     public static final String ITEM_URL="test.data.item.url";
     public static final String ITEM_NAME = "test.data.item.name";
-    public static final String ITEM_COLOR = "test.data.item.color";
     public static final String ITEM_SIZE = "test.data.item.size";
     public static final String ITEM_HEIGHT = "test.data.item.height";
     public static final String ITEM_PRICE = "test.data.item.price";
@@ -17,10 +16,10 @@ public class ItemCreator {
                 Double.parseDouble(TestDataReader.getTestData(ITEM_PRICE)));
     }
 
-    public static Item withEmptyCount(){
-        return new Item(TestDataReader.getTestData(ITEM_URL), TestDataReader.getTestData(ITEM_NAME),
-                TestDataReader.getTestData(ITEM_SIZE), TestDataReader.getTestData(ITEM_HEIGHT),
-                Double.parseDouble(TestDataReader.getTestData(ITEM_PRICE)));
-    }
+//    public static Item withEmptyCount(){
+//        return new Item(TestDataReader.getTestData(ITEM_URL), TestDataReader.getTestData(ITEM_NAME),
+//                TestDataReader.getTestData(ITEM_SIZE), TestDataReader.getTestData(ITEM_HEIGHT),
+//                Double.parseDouble(TestDataReader.getTestData(ITEM_PRICE)));
+//    }
 
 }
